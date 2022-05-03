c9400242a34d3f61d3e9c34516111cfc67f964e6
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-04 02:09:28 +0900

    모의투자 화면 매도화면 제외 작성완료

--- components/container/transaction/TransactionView.js
+++ components/container/transaction/TransactionView.js
@@ -1,8 +1,25 @@
 import Headers from "../../shared/Headers/container/Headers";
+import TransContentView from "./TransContentView";
+import TransSideView from "./TransSideView";
 
 function TransactionView() {
     return (
-        <Headers />
+        <div className=" w-container m-auto ">
+            <div className=" h-headers bg-white">
+                <Headers />
+            </div>
+            <div className=" bg-white flex">
+                <div className="  w-[387px] mr-5">
+                    <TransContentView />
+                </div>
+                <div className=" w-rankview ">
+                    <TransSideView />
+                </div>
+            </div>
+            <div className=" h-footers bg-green-500">
+                footer
+            </div>
+        </div>
     );
 }
 
