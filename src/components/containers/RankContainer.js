db8ce80717ed2a327c9e7cd6619511f05b6d20f8
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-08 02:02:06 +0900

    5. 차트 state 관리 로직 1. UTC 현재시간으로 변환 2. 현재 state의 time값과 변환한 값이 일치할 경우 변경 작업 3. 아닐경우 기존 state는 dispath하고 state를 갈아끼움 > 테스트 남음
    랭킹 페이지 뷰 작업 완료

---
+++ src/components/containers/RankContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rank from '../presentations/rank/rank';
+import Wrapper from "../presentations/Wrapper";
+
+function RankContainer() {
+    return (
+        <Wrapper>
+            <Rank/>
+        </Wrapper>
+    );
+}
+
+
+export default RankContainer;
--- src/components/presentations/transaction/TransChart.js
+++ src/components/presentations/transaction/TransChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import { useDispatch, useSelector } from "react-redux";
 import { useSelect } from "@mui/base";
@@ -10,6 +10,13 @@ const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 function TransChart() {
     const dispatch = useDispatch();
     const chartData = useSelector((state) => state.chart.getChart);
+    const [currentState,setCureentState] = useState({
+      time: '',
+      start : 0,
+      high: 0,
+      low: 0,
+      close: 0
+    });
     const coinType = 'KRW-BTC';
     const dState = {
         options: {
@@ -33,10 +40,28 @@ function TransChart() {
           }]
         };
     const DataSetting = (realData) => {
-      const inputData = {
-        x: realData.tradeTime,
-        y: [realData.tradePrice,realData.tradePrice,realData.tradePrice,realData.tradePrice]
+      
+      // 현재 state의 시간과 realData의 시간이 일치할 경우 
+      if(realData.tradeTime === currentState.time)
+      {
+        const newState = {
+          ...currentState,
+          high: realData.tradePrice > currentState.high ? realData.tradePrice : currentState.high,
+          low: realData.tradePrice < currentState.low ? realData.tradePrice : currentState.low,
+          close: realData.tradePrice
+        }
+        setCureentState(newState);
       }
+      // 일치하지 않을 경우 
+      else 
+      if(currentState.time !== 0)
+        {
+          const inputData = {
+            x: currentState.time,
+            y: [currentState.start,currentState.high,currentState.low,currentState.close]
+          }
+          dispatch(addChart(inputData))
+        }
     }
 
     React.useEffect(()=> {
