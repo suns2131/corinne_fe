d1a8eef32bca527e353d22d4de9f3fff0a991be2
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-13 22:24:15 +0900

    소켓 연결 주소변경

--- src/share/socket.js
+++ src/share/socket.js
@@ -1,7 +1,8 @@
 import SockJS from 'sockjs-client'
 import Stomp from 'stompjs'
 
-const Socket = SockJS("https://gyuwony.shop:8090/stomp");
+// const Socket = SockJS("https://gyuwony.shop:8090/stomp");
+const Socket = SockJS("http://13.125.232.165:8090/stomp");
 const socketClient = Stomp.over(Socket);
 
 export default socketClient;
