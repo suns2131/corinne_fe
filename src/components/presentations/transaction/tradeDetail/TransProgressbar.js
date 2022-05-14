2369fa6c95727d114480372126e6bddfe8923a6e
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-14 22:20:18 +0900

    사용하지 않는 라이브러리 제거 필요없는 파일 정보 제거

--- src/components/presentations/transaction/tradeDetail/TransProgressbar.js
+++ src/components/presentations/transaction/tradeDetail/TransProgressbar.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
-import Slider from '@mui/material/Slider';
+// import Slider from '@mui/material/Slider';
 import { useDispatch, useSelector } from 'react-redux';
-import {createTheme, ThemeProvider} from '@mui/material/styles'
+// import {createTheme, ThemeProvider} from '@mui/material/styles'
 import { updateSell } from '../../../../state/reducer/transaction/trans';
 
 function TransProgressbar({ buyRequest, setBuyRequest ,sellRequest ,setSellRequest, type, userAmount, setSellPrice}) {
@@ -9,16 +9,16 @@ function TransProgressbar({ buyRequest, setBuyRequest ,sellRequest ,setSellReque
     const marker = [{value : 1},{value: 25},{value: 50,},{value: 75,},{value: 100,}]
     const currentMount = useSelector((state) => state.chart.getCurrentMonut);
     const [leverage, setLeverage] = React.useState(1);
-    const sliderTheme = createTheme({
-      palette : {
-        customPurple: {
-          main: '#6800BA'
-        },
-        customOrange: {
-          main: '#FF9E0D',
-        }
-      }
-    })
+    // const sliderTheme = createTheme({
+    //   palette : {
+    //     customPurple: {
+    //       main: '#6800BA'
+    //     },
+    //     customOrange: {
+    //       main: '#FF9E0D',
+    //     }
+    //   }
+    // })
 
     useEffect(() => {
       if(userAmount?.coins !== undefined)
@@ -79,7 +79,7 @@ function TransProgressbar({ buyRequest, setBuyRequest ,sellRequest ,setSellReque
         {type === "buy" ?
         <div className='w-[339px] flex justify-between items-center text-[14px] mb-[18px]'>
           <p className='w-[80px] '>레버리지</p>
-          <ThemeProvider theme={sliderTheme}>
+          {/* <ThemeProvider theme={sliderTheme}>
           <Slider
             color = "customPurple"
             sx={{ width: 213, height: 7}}
@@ -92,13 +92,13 @@ function TransProgressbar({ buyRequest, setBuyRequest ,sellRequest ,setSellReque
             valueLabelDisplay="auto"
             marks={marker}
             />
-          </ThemeProvider>
+          </ThemeProvider> */}
           <p className=' w-[15px] text-[#6800BA] text-[15px] font-bold'>{leverage}x</p>
         </div>
         :
         <div className='w-[339px] flex justify-between items-center text-[14px] mb-[18px]'>
           <p className='w-[80px] '>레버리지</p>
-          <ThemeProvider theme={sliderTheme}>
+          {/* <ThemeProvider theme={sliderTheme}>
           <Slider
             color = "customOrange"
             sx={{ width: 213, height: 7}}
@@ -111,7 +111,7 @@ function TransProgressbar({ buyRequest, setBuyRequest ,sellRequest ,setSellReque
             valueLabelDisplay="auto"
             marks={marker}
             />
-          </ThemeProvider>
+          </ThemeProvider> */}
           <p className=' w-[15px] text-[#FF9E0D] text-[15px] font-bold'>{leverage}x</p>
         </div>
         }
