cc6392198304cd0b46b378a29e1491902a2d5fef
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-16 13:57:06 +0900

    랭킹페이지 뷰작업 완료

--- src/components/containers/RankContainer.js
+++ src/components/containers/RankContainer.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import FollowRank from '../presentations/rank/followrank/FollowRank';
 import MatchOne from '../presentations/rank/match/match';
 import PrevRank from '../presentations/rank/prevrank/PrevRank';
+import PrevModal from '../presentations/rank/rankmodal/PrevModal';
 import RealRank from '../presentations/rank/realrank/RealRank';
 import Userinfo from '../presentations/rank/userinfo/UserInfo';
 import Wrapper from '../presentations/Wrapper';
@@ -24,18 +25,8 @@ function RankContainer() {
         </div>
       </Wrapper>
       {modal && (
-        <div
-          className="w-full h-screen fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-40 text-center z-40"
-          onClick={() => {
-            setModal((prev) => !prev);
-          }}
-          onKeyDown={() => {
-            setModal((prev) => !prev);
-          }}
-          role="button"
-          tabIndex="0"
-        >
-          <div className="w-[600px] h-[600px] bg-red-400 absolute">sdf</div>
+        <div className="w-full h-screen fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-40 text-center z-40">
+          <PrevModal setModal={setModal} />
         </div>
       )}
     </div>
