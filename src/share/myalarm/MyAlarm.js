54f1d904d6bd3c419624e07a95750fc99bc4064a
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-17 03:10:24 +0900

    AGOI-19 알림컴포넌트
    내소식 컴포넌트

---
+++ src/share/myalarm/MyAlarm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyAlarm() {
+  return (
+    <div className="w-[440px] h-[560px] flex flex-col items-end relative">
+      <div>123</div>
+    </div>
+  );
+}
+
+export default MyAlarm;
