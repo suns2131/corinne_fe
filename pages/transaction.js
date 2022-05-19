dcd0160cf48dca6f26dbc4bfe22e999533ad7a5f
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-19 21:04:43 +0900

    2차 오류수정

--- pages/transaction.js
+++ pages/transaction.js
@@ -1,13 +1,13 @@
-import head from "next/head"
-import TransContainer from "../src/components/containers/TransContainer";
+import head from 'next/head';
+import TransContainer from '../src/components/containers/TransContainer';
 
 export default function Transaction() {
-    return (
-      <div>
+  return (
+    <div>
       <head>
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       </head>
       <TransContainer />
-      </div>
-    );
+    </div>
+  );
 }
