e5f3c9e541fbad12e54a364de0bc8bbbefe268ae
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-19 21:18:04 +0900

    오류 수정

--- src/components/presentations/transaction/TransSocket.js
+++ src/components/presentations/transaction/TransSocket.js
@@ -1,7 +1,7 @@
 import sockjs from 'sockjs-client';
 import Stomp from 'stompjs';
 
-const socket = sockjs('http://sparta-jm.site/stomp');
+const socket = sockjs('https://sparta-jm.site/stomp');
 const stpClient = Stomp.over(socket);
 
 export default stpClient;
--- src/share/socket.js
+++ src/share/socket.js
@@ -1,7 +1,7 @@
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
-const Socket = SockJS('sparta-jm.site:8090/stomp');
+const Socket = SockJS('https://sparta-jm.site:8090/stomp');
 const socketClient = Stomp.over(Socket);
 
 export default socketClient;
