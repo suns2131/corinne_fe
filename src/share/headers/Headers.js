078143e49b73459127b66df49f13e171edb0e8be
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-22 21:23:44 +0900

    test

--- src/share/headers/Headers.js
+++ src/share/headers/Headers.js
@@ -190,7 +190,7 @@ export default function Headers({ handleRouter, headerMenu }) {
                   key={key}
                   role="presentation"
                   onClick={handleRouter(pathname)}
-                  className="w-[68px] h-[36px] grow-0 flex justify-center items-center gap-[10px] px-[8px] cursor-pointer"
+                  className="w-[90px] h-[36px] grow-0 flex justify-center items-center gap-[10px] px-[8px] cursor-pointer"
                 >
                   <span
                     className={cn(
