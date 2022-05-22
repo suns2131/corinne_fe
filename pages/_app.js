e0d562a117b3ac37d3e1718743effdd46f2c4c6b
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-23 07:41:41 +0900

    구글 애널리틱스 설정

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import wrapper from '../src/state';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
