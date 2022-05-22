b5da3a171b33def4426d00a9ca20e25c596052aa
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-22 21:41:45 +0900

    level width 수정

--- src/share/Level.js
+++ src/share/Level.js
@@ -32,14 +32,14 @@ function Level({ Exp }) {
       {colorName.length > 2 ? (
         <div className="w-[84px] h-[26px] flex justify-center items-center gap-[4px] p-[6px,10px] rounded-[20px] border border-solid border-[#e4e4e4] bg-Neutrals-white ">
           <div className={`w-[14px] h-[14px] ${colors} rounded-full flex-grow-0`} />
-          <span className="w-[46px] h-[20px] flex-grow-0 flex justify-center items-center font-Pretendard text-[12px] text-left text-Neutrals-black">
+          <span className="w-[66px] h-[20px] flex-grow-0 flex justify-center items-center font-Pretendard text-[12px] text-left text-Neutrals-black">
             Lv.{colorName}
           </span>
         </div>
       ) : (
         <div className="w-[74px] h-[26px] flex justify-center items-center gap-[4px] p-[6px,10px] rounded-[20px] border border-solid border-[#e4e4e4] bg-Neutrals-white ">
           <div className={`w-[14px] h-[14px] ${colors} rounded-full flex-grow-0`} />
-          <span className="w-[36px] h-[20px] flex-grow-0 flex justify-center items-center font-Pretendard text-[12px] text-left text-Neutrals-black">
+          <span className="w-[56px] h-[20px] flex-grow-0 flex justify-center items-center font-Pretendard text-[12px] text-left text-Neutrals-black">
             Lv.{colorName}
           </span>
         </div>
