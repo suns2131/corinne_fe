671ce8f524a6267d93562fc69ae60658c999aece
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-23 09:13:17 +0900

    result rank알람 추가

--- src/share/myalarm/Alarms.js
+++ src/share/myalarm/Alarms.js
@@ -5,7 +5,7 @@ import AlarmNone from '../../../public/icons/header/alarm_none.svg';
 function Alarms({ alData }) {
   if (!alData) return null;
   console.log(alData);
-  if (alData.alarmNo === 'RESULT') {
+  if (alData.alarmNo === 'RANK') {
     return (
       <div className="w-[440px] h-[108px] flex-grow-0 flex flex-col justify-center items-stretch gap-[10px] px-5 bg-Neutrals-white">
         <div className="h-[32px] self-stretch flex-grow-0 flex justify-end items-center gap-[4px]">
@@ -86,6 +86,49 @@ function Alarms({ alData }) {
         </div>
       </div>
     );
+  if (alData.alarmNo === 'RESULT') {
+    return (
+      <div className="w-[440px] h-[108px] flex-grow-0 flex flex-col justify-center items-stretch gap-[10px] px-5 bg-Neutrals-white">
+        <div className="h-[32px] self-stretch flex-grow-0 flex justify-end items-center gap-[4px]">
+          <Image
+            src="/images/defaultProfile/defalutProfile32.png"
+            alt="deflutProfile"
+            width={32}
+            height={32}
+          />
+          <div className="flex flex-col justify-start items-center">
+            <span className="ml-[16px] w-[291px] flex-grow-0 font-Pretendard text-[15px] text-Neutrals-black ">
+              <span className="font-bold">주간 모의투자 랭킹 결과 </span>회원님은
+              <span> {alData.content}</span> 입니다.
+            </span>
+            {alData.content === '1위' && (
+              <span className="text-[15px] text-Neutrals-gray">
+                경험치 30,000xp / 원화 2,000,000원
+              </span>
+            )}
+            {alData.content === '2위' && (
+              <span className="text-[15px] text-Neutrals-gray">
+                경험치 20,000xp / 원화 1,500,000원
+              </span>
+            )}
+            {alData.content === '3위' && (
+              <span className="text-[15px] text-Neutrals-gray">
+                경험치 10,000xp / 원화 1,200,000원
+              </span>
+            )}
+            {alData.content === '참여자' && (
+              <span className="text-[15px] text-Neutrals-gray">
+                경험치 5,000xp(참여자 전원 지급)
+              </span>
+            )}
+          </div>
+          <span className="w-[44px] h-[23px] flex-grow-0 font-Pretendard text-[15px] text-right text-Neutrals-lightGray">
+            {alData.createAT}
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Alarms;
