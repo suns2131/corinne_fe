4afdc11b005a7efdd8f53f2a79d43a80964856d1
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-26 19:51:36 +0900

    점검 추가

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import HomeContainer from '../src/components/containers/HomeContainer';
+import Wrapper from '../src/components/presentations/Wrapper';
 //  import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <HomeContainer />;
+  // return <HomeContainer />;
+  return (
+    <Wrapper>
+      <div className="w-full h-full flex justify-center items-center text-Neutrals-white text-[3em]">
+        <span className="mt-[5em]">서비스 점검중입니다.</span>
+      </div>
+    </Wrapper>
+  );
 }
