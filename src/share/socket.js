20b7abdbb050119e14889bba7462d7cc74887f44
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-26 19:45:38 +0900

    소켓 변경

--- src/share/socket.js
+++ src/share/socket.js
@@ -1,8 +1,8 @@
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
-const Socket = SockJS('https://sparta-jm.site/stomp');
-// const Socket = SockJS('http://52.78.173.104:8080/stomp');
+const Socket = SockJS('https://gyuwony.shop/stomp');
+// const Socket = SockJS('https://sparta-jm.site/stomp');
 const socketClient = Stomp.over(Socket);
 
 export default socketClient;
