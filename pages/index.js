834f93c7c2f4f9414e1df143a17945991e3b05b9
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-05-26 20:35:49 +0900

    점검 해제

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
 import HomeContainer from '../src/components/containers/HomeContainer';
-import Wrapper from '../src/components/presentations/Wrapper';
-//  import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  // return <HomeContainer />;
-  return (
-    <Wrapper>
-      <div className="w-full h-full flex justify-center items-center text-Neutrals-white text-[3em]">
-        <span className="mt-[5em]">서비스 점검중입니다.</span>
-      </div>
-    </Wrapper>
-  );
+  return <HomeContainer />;
 }
